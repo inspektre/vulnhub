e3e15d6aee1e3ddc82e7afbe840fa6d8aaf2cfc2
Uday Korlimarla <uday@inspektre.io>
2020-10-20 19:14:11 +1100

    seed invoke

--- src/seed.js
+++ src/seed.js
@@ -13,14 +13,4 @@ import { runCveMutations } from './transforms';
 // CVE Mutations
 console.log("Initializgin CVE Seed Mutations");
 
-const iterCves = async () => {
-    let currentYear;
-    const maxYear = new Date().getFullYear();
-    
-    for (currentYear = 2002; currentYear <= maxYear; currentYear++) {
-        const complete = await runCveMutations(currentYear);
-        console.log(complete);
-    }
-};
-
-iterCves();
+runCveMutations();
