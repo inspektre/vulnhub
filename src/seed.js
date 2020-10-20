e275abcc2d11bcfc7fa4488880e88d84633d0233
Uday Korlimarla <uday@inspektre.io>
2020-10-20 21:10:41 +1100

    fixed

--- src/seed.js
+++ src/seed.js
@@ -13,15 +13,15 @@ import { runCveMutations } from './transforms';
 // CVE Mutations
 console.log("Initializgin CVE Seed Mutations");
 
-const iterCves = async (year) => {
+const iterCves = async () => {
     // const complete = await runCveMutations(year);
     // console.log("Completed ", year);
     let currentYear;
     const maxYear = new Date().getFullYear();
     for (currentYear = 2002; currentYear <= maxYear; currentYear++) {
         const complete = await runCveMutations(currentYear);
-        console.log(complete);
+        // console.log(complete);
     }
 };
 
-iterCves(2002);
+iterCves();
