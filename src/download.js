2a11b4b563418289b375f42433eacebfe27c5cab
Uday Korlimarla <uday@inspektre.io>
2020-10-22 01:32:02 +1100

    download feeds

---
+++ src/download.js
@@ -0,0 +1,8 @@
+/*
+Author: Uday Korlimarla
+Copyright (c) Inspektre Pty Ltd
+*/
+import { getJsonCveFeeds } from './downloadFeeds';
+
+// Download CVE Feeds
+getJsonCveFeeds();
--- src/seed.js
+++ src/seed.js
@@ -2,13 +2,7 @@
 Author: Uday Korlimarla
 Copyright (c) Inspektre Pty Ltd
 */
-import { getJsonCveFeeds } from './downloadFeeds';
 import { runCveMutations } from './transforms';
-
-
-
-// Download CVE Feeds
-// getJsonCveFeeds();
 
 // CVE Mutations
 console.log("Initializgin CVE Seed Mutations");
