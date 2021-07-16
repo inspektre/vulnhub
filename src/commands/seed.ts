2befbbb4a0acbd99f8e9cc39b71ab8aafffff898
Uday Korlimarla <44824134+udaykor@users.noreply.github.com>
2021-07-16 19:43:13 +1000

    add update plugin

--- src/commands/seed.ts
+++ src/commands/seed.ts
@@ -1,57 +1,28 @@
 import {Command, flags} from '@oclif/command'
 import { seed } from '../utils/seed';
-import { createIndices, createGraphs } from '../utils/cveHelpers';
+import { createIndices } from '../utils/cveHelpers';
 
 export default class Seed extends Command {
   static description = 'Seed NVD CVEs to Neo4J'
 
   static examples = [
-    `$ vulnhub seed --ensure-index
-    CVE node indices will be crated/checked
-    Feed: 2006, Location: /home/user/.config/inspektre/feeds/cve/nvdcve-1.1-2006.json.gz
-    Feed: 2007, Location: /home/user/.config/inspektre/feeds/cve/nvdcve-1.1-2007.json.gz
-    `,
-    `$ vulnhub seed --create-graphs
-    CVE node indices will be crated/checked
-    Feed: 2006, Location: /home/user/.config/inspektre/feeds/cve/nvdcve-1.1-2006.json.gz
-    Feed: 2007, Location: /home/user/.config/inspektre/feeds/cve/nvdcve-1.1-2007.json.gz
-    `,
-    `$ vulnhub seed --ensure-index --create-graphs
-    CVE node indices will be crated/checked
-    Feed: 2006, Location: /home/user/.config/inspektre/feeds/cve/nvdcve-1.1-2006.json.gz
-    Feed: 2007, Location: /home/user/.config/inspektre/feeds/cve/nvdcve-1.1-2007.json.gz
-    `,
     `$ vulnhub seed
+    CVE node indices will be crated/checked
     Feed: 2006, Location: /home/user/.config/inspektre/feeds/cve/nvdcve-1.1-2006.json.gz
     Feed: 2007, Location: /home/user/.config/inspektre/feeds/cve/nvdcve-1.1-2007.json.gz
     `,
   ];
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    'ensure-index': flags.boolean({char: 'e'}),
-    'create-graphs': flags.boolean({ char: 'c'})
+    help: flags.help({char: 'h'})
   };
 
   static args = [];
 
   async run() {
     const {args, flags} = this.parse(Seed);
+    await createIndices();
     await seed();
-    if(flags['create-graphs'] && flags['ensure-index']) {
-      this.log('CVE node indices will be crated/checked.');
-      await createIndices();
-      this.log('CVE node graphs will be created/checked.');
-      await createGraphs();
-    }
-    else if(flags['ensure-index']) {
-      this.log('CVE node indices will be crated/checked.');
-      await createIndices();
-    }
-    else if(flags['create-graphs']) {
-      this.log('CVE node graphs will be created/checked.');
-      await createGraphs();
-    }
     this.exit(0);
   }
 }
